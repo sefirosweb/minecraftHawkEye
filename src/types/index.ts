9b2f804dee8c945e17466daf7687e5ed6e0f879e
Sefirosweb <sefirosweb@gmail.com>
2023-02-21 19:48:49 +0000

    Fix lint

--- src/types/index.ts
+++ src/types/index.ts
@@ -102,6 +102,9 @@ declare module 'mineflayer' {
         hawkEye: HawkEye
     }
     interface BotEvents extends HawkEyeEvents {
+        auto_shot_stopped: (target: Entity | OptionsMasterGrade) => void
+        target_aiming_at_you: (entity: Entity, arrowTrajectory: Array<Vec3>) => void
+        incoming_projectil: (projectil: Projectil, arrowTrajectory: Array<Vec3>) => void
     }
 }
 
